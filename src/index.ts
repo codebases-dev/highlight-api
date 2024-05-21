aa611f4ee688fc5aa957615459e39c37f490c1d3
neoki <ot07neoki@gmail.com>
2024-05-21 22:55:07 +0900

    feat: add batch endpoint

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { Hono } from "hono";
 import { type HighlighterCore, getHighlighterCore, loadWasm } from "shiki/core";
 import js from "shiki/langs/javascript.mjs";
 import theme from "shiki/themes/github-dark-dimmed.mjs";
-import { codeSchema } from "./schema";
+import { codeSchema, codesSchema } from "./schema";
 
 // @ts-ignore
 await loadWasm(import("shiki/onig.wasm"));
@@ -36,6 +36,43 @@ app.post(
 					lang: language,
 				}),
 			});
+		} catch (error) {
+			return c.json({ error: "An unexpected error occurred" }, 500);
+		}
+	},
+);
+
+app.post(
+	"/highlight/batch",
+	zValidator("json", codesSchema, (result, c) => {
+		if (!result.success) {
+			return c.json({ error: "Invalid input" }, 400);
+		}
+	}),
+	async (c) => {
+		try {
+			const { codes } = c.req.valid("json");
+
+			const results = await Promise.all(
+				codes.map(async ({ code, language }, index) => {
+					if (!highlighter) {
+						highlighter = await getHighlighterCore({
+							themes: [theme],
+							langs: [js],
+						});
+					}
+
+					return {
+						index,
+						html: highlighter.codeToHtml(code, {
+							theme: "github-dark-dimmed",
+							lang: language,
+						}),
+					};
+				}),
+			);
+
+			return c.json({ results });
 		} catch (error) {
 			return c.json({ error: "An unexpected error occurred" }, 500);
 		}
--- src/schema.ts
+++ src/schema.ts
@@ -4,3 +4,7 @@ export const codeSchema = z.object({
 	code: z.string(),
 	language: z.string(),
 });
+
+export const codesSchema = z.object({
+	codes: z.array(codeSchema),
+});
